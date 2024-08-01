8109993bc7a6cbb270b5bd97dbd45d347179b000
6akov <egorka.shestakov.2017@mail.ru>
2024-08-01 18:46:03 +0300

    PersonDAO realised

--- src/main/java/org/example/controllers/PeopleController.java
+++ src/main/java/org/example/controllers/PeopleController.java
@@ -1,5 +1,6 @@
 package org.example.controllers;
 
+import org.example.dao.PersonDAO;
 import org.example.models.Person;
 <<<<<<< HEAD
 =======
--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -1,8 +1,39 @@
 package org.example.dao;
 
+import org.example.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PersonDAO {
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
+    public List<Person> getPeople() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person getPerson(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void addPerson(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(fullName, yearOfBirth) VALUES(?, ?)", person.getFullName(), person.getYearOfBirth());
+    }
+
+    public void updatePerson(int id, Person person) {
+        jdbcTemplate.update("UPDATE Person SET fullName=?, yearOfBirth=? WHERE id=?", person.getFullName(), person.getYearOfBirth(), id);
+    }
+
+    public void deletePerson(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
 }
