dea8849eaec5313cc26b0e16138bcad515987da4
6akov <egorka.shestakov.2017@mail.ru>
2024-08-02 09:47:53 +0300

    BookDAO

--- src/main/java/org/example/dao/BookDAO.java
+++ src/main/java/org/example/dao/BookDAO.java
@@ -1,6 +1,7 @@
 package org.example.dao;
 
 import org.example.models.Book;
+import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,5 +20,27 @@ public class BookDAO {
 
     public List<Book> getBooksOfPerson(int personId) {
         return jdbcTemplate.query("SELECT * FROM Book WHERE person_id=?", new Object[]{personId}, new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public List<Book> getBooks() {
+        return jdbcTemplate.query("SELECT * FROM Book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book getBook(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public Person getPersonByBook(int bookId) {
+        return jdbcTemplate.query("SELECT Person FROM Book JOIN Person ON Person.person_id = Book.person_id WHERE book_id=?", new Object[]{bookId}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void addBook(Book book) {
+        jdbcTemplate.update("INSERT INTO Book(name, author, yearProduction) VALUES(?, ?, ?)", book.getName(), book.getAuthor(), book.getYearProduction());
+    }
+
+    public void deleteBook(int id) {
+        jdbcTemplate.update("DELETE FROM Book WHERE id=?", id);
     }
 }
