88c332fd564bf4dba32cd1e23a6b6265a12ab945
6akov <egorka.shestakov.2017@mail.ru>
2024-08-02 09:12:06 +0300

    BooksController

---
+++ src/main/java/org/example/controllers/BooksController.java
@@ -0,0 +1,51 @@
+package org.example.controllers;
+
+import org.example.dao.BookDAO;
+import org.example.dao.PersonDAO;
+import org.example.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller()
+@RequestMapping("/books")
+public class BooksController {
+    private BookDAO bookDAO;
+    private PersonDAO personDAO;
+    @Autowired
+    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
+        this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String books(Model model) {
+        model.addAttribute("books", bookDAO.getBooks());
+        return "books/books";
+    }
+
+    @GetMapping("/{id}")
+    public String book(Model model, @PathVariable int id) {
+        model.addAttribute("book", bookDAO.getBook(id));
+        model.addAttribute("person", bookDAO.getPersonByBook(id));
+        return "books/book";
+    }
+
+    @PostMapping("/new")
+    public String addBook(@ModelAttribute Book book) {
+        bookDAO.addBook(book);
+        return "redirect:/books";
+    }
+
+    @PatchMapping("/{id}")
+    public String freeBook(@PathVariable int id) {
+        return "redirect"; // ?????
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable int id) {
+        bookDAO.deleteBook(id);
+        return "redirect:/books";
+    }
+}
